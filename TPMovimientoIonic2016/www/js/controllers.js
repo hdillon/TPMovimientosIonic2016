b395977d792d3909f70b784b7bb0a14da324868f
hdillon <dillonhoraciodavid@gmail.com>
2016-10-02 18:04:40 -0300

    Grabado de sonidos por el usuario

--- TPMovimientoIonic2016/www/js/controllers.js
+++ TPMovimientoIonic2016/www/js/controllers.js
@@ -29,17 +29,130 @@ angular.module('starter.controllers', [])
 })
 
 
-.controller('MovimientoCtrl', function($scope, $state, $stateParams) {
+.controller('MovimientoCtrl', function($scope, $state, $stateParams, $cordovaMedia, $ionicPlatform) {
   $scope.usuario = angular.fromJson($stateParams);
   /*alert($scope.usuario.nombre);
   alert($scope.usuario.apellido);*/
-  $scope.itemOnLongPress = function(id) {
-    console.log('Long press');
+  $scope.itemOnLongPress = function(idSonido) {
+    console.log("adasdasdas");
+    switch(idSonido){
+      case 'arriba':
+      try{
+        console.log("entro arriba");
+        $scope._src = "arriba.mp3";
+        $scope.mediaArriba = $cordovaMedia.newMedia($scope._src);
+        $scope.mediaArriba.startRecord();
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'abajo':
+      try{
+        $scope._src = "abajo.mp3";
+        $scope.mediaAbajo = $cordovaMedia.newMedia($scope._src);
+        $scope.mediaAbajo.startRecord();
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'izquierda':
+      try{
+        $scope._src = "izquierda.mp3";
+        $scope.mediaIzquierda = $cordovaMedia.newMedia($scope._src);
+        $scope.mediaIzquierda.startRecord();
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'derecha':
+      try{
+        $scope._src = "derecha.mp3";
+        $scope.mediaDerecha = $cordovaMedia.newMedia($scope._src);
+        $scope.mediaDerecha.startRecord();
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      default:
+      break;
+    }
+  
+
   }
 
-  $scope.itemOnTouchEnd = function(id) {
-    console.log('Touch end');
+  $scope.itemOnTouchEnd = function(idSonido) {
+    switch(idSonido){
+      case 'arriba':
+      try{
+        console.log("stopArriba");
+        $scope.mediaArriba.stopRecord();
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'abajo':
+      try{
+        $scope.mediaAbajo.stopRecord();
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'izquierda':
+      try{
+        $scope.mediaIzquierda.stopRecord();
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'derecha':
+      try{
+        $scope.mediaDerecha.stopRecord();
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      default:
+      break;
+    } 
   }
+
+/* TEST SONIDOS
+  $scope.reproducir = function(idSonido){
+    switch(idSonido){
+      case 'arriba':
+      try{
+        console.log("reproducir arriba");
+        $scope.mediaArriba.play(); // Android
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'abajo':
+      try{
+        $scope.mediaAbajo.play(); // Android
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'izquierda':
+      try{
+        $scope.mediaIzquierda.play(); // Android
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      case 'derecha':
+      try{
+        $scope.mediaDerecha.play(); // Android
+      }catch(error){
+        console.log(error);
+      }
+      break;
+      default:
+      break;
+    } 
+  }
+*/
 
 })
 
